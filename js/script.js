afeefb85ed61628d54e039c6f4d0f04e941bc42e
Paulina Szamraj <paulinaps.93@wp.pl>
2023-04-13 13:52:16 +0200

    added js and example task

--- js/script.js
+++ js/script.js
@@ -1,3 +1,28 @@
 {
-    
+    const tasks = [
+        {
+            content: "test",
+        }
+    ];
+
+    const render = () => {
+        let htmlString = "";
+
+        for (const task of tasks) {
+            htmlString += `
+            <li>
+            ${task.content}
+            </li>
+            `;
+        }
+
+        document.querySelector(".js-tasks").innerHTML = htmlString;
+    };
+
+    const init = () => {
+        render();
+
+    };
+
+    init();
 }
