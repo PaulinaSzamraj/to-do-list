26af44ab2ddc879ce9b3d01895a3495b69d14a5b
Paulina Szamraj <paulinaps.93@wp.pl>
2023-04-13 14:02:28 +0200

    added button done and remove

--- js/script.js
+++ js/script.js
@@ -10,8 +10,16 @@
 
         for (const task of tasks) {
             htmlString += `
-            <li>
-            ${task.content}
+            <li class="tasks__item">
+            <button class="tasks__button tasks__button--toggleDone js-done">
+                ${task.done ? "✔" : ""}
+            </button>
+            <span class="tasks__content ${task.done ? "tasks__content--done" : ""}">
+                ${task.content}
+            </span>
+            <button class="tasks__button tasks__button--remove js-remove">
+                🗑
+            </button>
             </li>
             `;
         }
