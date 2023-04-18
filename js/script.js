b7b87d7775c36a72de7af2c854aabc21436a4b59
Paulina Szamraj <paulinaps.93@wp.pl>
2023-04-18 17:52:00 +0200

    added indentation, removed blank line

--- js/script.js
+++ js/script.js
@@ -42,17 +42,17 @@
 
         for (const task of tasks) {
             htmlString += `
-            <li class="tasks__item">
-            <button class="tasks__button tasks__button--toggleDone js-done">
-                ${task.done ? "✔" : ""}
-            </button>
-            <span class="tasks__content ${task.done ? "tasks__content--done" : ""}">
-                ${task.content}
-            </span>
-            <button class="tasks__button tasks__button--remove js-remove">
-                🗑
-            </button>
-            </li>
+                <li class="tasks__item">
+                    <button class="tasks__button tasks__button--toggleDone js-done">
+                        ${task.done ? "✔" : ""}
+                    </button>
+                    <span class="tasks__content ${task.done ? "tasks__content--done" : ""}">
+                        ${task.content}
+                    </span>
+                    <button class="tasks__button tasks__button--remove js-remove">
+                        🗑
+                    </button>
+                </li>
             `;
         }
 
@@ -73,7 +73,6 @@
         }
 
         newTaskElement.focus();
-
     };
 
     const init = () => {
